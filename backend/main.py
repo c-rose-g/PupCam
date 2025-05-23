ba447191ee2827e3b74ca122d55661b5fc9e7a4e
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-22 22:19:44 -0700

    Update main.py
    I moved the code to database.py

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,4 @@
-# add mongoDB + Beanie setup
-from fastapi import FastAPI
-from beanie import init_beanie
-# from models.event import Event
-from motor.motor_asyncio import AsyncIOMotorClient
-from dotenv import load_dotenv
-import os
+import uvicorn
 
-load_dotenv()
-MONGO_URL = os.getenv("MONGO_URL")
-DB_NAME = os.getenv("DB_NAME", "pupcam")
-app = FastAPI()
-
-@app.get("/")
-async def root():
-  return {"message": "Hello World"}
+if __name__ == "__main__":
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
