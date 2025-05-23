7c9604c86091d9d555603a66bb8f283eba6e6736
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-23 01:41:56 -0700

    Update events.py
    remove response_model parameter, added get_event_by_id function

--- backend/server/routes/events.py
+++ backend/server/routes/events.py
@@ -1,9 +1,20 @@
 from fastapi import APIRouter, HTTPException
-from typing import List
+from typing import List, Optional, Dict
+from beanie import PydanticObjectId
 from server.models.event import Event
 
-router = APIRouter("/events", tags=["Events"])
+router = APIRouter(prefix="/events", tags=["Events"])
 
-@router.get("/", response_model=List[Event])
+@router.get("/")
 async def get_all_events():
   return await Event.find_all().to_list()
+
+@router.get("/{event_id}")
+async def get_event_by_id(event_id: str):
+  try:
+    obj_id = PydanticObjectId(event_id)
+  except Exception:
+    raise HTTPException(status_code=422, detail="Invalid ObjectId format")
+
+  event = await Event.get(obj_id)
+  return event
