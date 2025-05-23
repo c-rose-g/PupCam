46ffa1bcd22c766be276ceda86831065ec2310ae
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-23 01:39:12 -0700

    Update database.py
    I explicitly state where to find the .env file, fix the DB_NAME name,

--- backend/server/database.py
+++ backend/server/database.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
@@ -7,13 +8,27 @@ from server.models.event import Event
 # from server.models.user import User
 # from server.models.camera import Camera
 
-load_dotenv()
+env_path = Path(__file__).resolve().parents[1].parents[0].parents[0] / "PupCam/.env"
 
+load_dotenv(dotenv_path=env_path)
+DB_NAME = os.getenv("DB_NAME")
 MONGO_URI = os.getenv("MONGO_URI")
-DB_NAME = os.getenv("DV_NAME")
+
+if not MONGO_URI:
+    raise ValueError("MONGO_URI is not set in env")
+
+if not DB_NAME:
+    raise ValueError("DB_NAME is not set in env")
+
 
 async def init_db():
-  client = AsyncIOMotorClient(MONGO_URI)
-  db = client[DB_NAME]
+    client = AsyncIOMotorClient(MONGO_URI)
+    db = client[DB_NAME]
 
-  await init_beanie(database=db, document_models=[Event])
+    # add the rest of the models here
+    try:
+        await init_beanie(database=db, document_models=[Event])
+
+    except Exception as e:
+        print("Beanie init failed:        ", e)
+        raise
