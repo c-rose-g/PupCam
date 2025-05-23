f11cf9a3105b4fe0369e481d598490bbdf084e18
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-23 01:35:43 -0700

    Update main.py
    I'm using `uvicorn main:app --reload` to initiate the backend server, so I don't need this code anymore.

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,4 @@ app.include_router(event_router)
 
 @app.get("/")
 async def root():
-    return {"message:":"Hello World"}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    return {"message:": "Welcome to the PupCam backend server"}
