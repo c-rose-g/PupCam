5b50ad9e53338ec3d42d90cae69a37089fed48b1
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-22 23:54:44 -0700

    Update main.py
    I changed my entry point from backend/server/app.py to backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,21 @@
 import uvicorn
+from fastapi import FastAPI
+from server.database import init_db
+from contextlib import asynccontextmanager
+from server.routes import event_router
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(event_router)
+
+@app.get("/")
+async def root():
+    return {"message:":"Hello World"}
 
 if __name__ == "__main__":
-    uvicorn.run("server.app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
