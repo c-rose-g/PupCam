e8e32772771c718de1d9b4101a6140cfdacbd23b
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-23 20:31:07 -0700

    Update users routes

--- backend/server/routes/users.py
+++ backend/server/routes/users.py
@@ -7,5 +7,14 @@ router = APIRouter(prefix="/users", tags=["Users"])
 
 @router.get("/")
 async def get_all_users():
-  # return {"message":"you've reached the user endpoint."}
   return await User.find_all().to_list()
+
+@router.post("/")
+async def create_user(user: User):
+  await User.create()
+  return user
+
+@router.get("/{user_id}")
+async def get_user_by_id():
+  user = await User.find_one(user_id = str)
+  return user
