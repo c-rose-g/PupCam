07bc40f8ead2439c90418f2bdd9a17c4cd2d4275
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-24 02:16:36 -0700

    Update main.py
    I added the device router and OAuth2

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,13 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
 from server.database import init_db
 from contextlib import asynccontextmanager
-from server.routes import event_router
+from server.routes import device_router
 from server.routes import user_router
+from server.routes import event_router
+from typing import Annotated
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,10 +15,15 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-app.include_router(event_router)
+
 app.include_router(user_router)
+app.include_router(device_router)
+app.include_router(event_router)
 
 @app.get("/")
-async def root():
-    return {"message:": "Welcome to the PupCam backend server. Please use the http://127.0.0.1:8000/docs endpoint to see the API documentation."}
+async def root(token: Annotated[str, Depends(oauth2_scheme)]):
+
+    return {"message:": "Welcome to the PupCam backend server. Please use the http://127.0.0.1:8000/docs endpoint to see the API documentation.",
+            "token": token}
