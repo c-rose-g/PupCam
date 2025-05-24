d095646bed0ae84ec6cc7a36d379a179dc4f44a1
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-24 02:19:31 -0700

    Update __init__.py

--- backend/server/routes/__init__.py
+++ backend/server/routes/__init__.py
@@ -1,4 +1,5 @@
+from .devices import router as device_router
 from .events import router as event_router
 from .users import router as user_router
 
-__all__ = ["event_router", "user_router"]
+__all__ = ["user_router","device_router", "event_router"]
