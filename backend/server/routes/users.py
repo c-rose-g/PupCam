8fdb7bc69978f79fa187c645452515518df9d5b7
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-23 20:25:29 -0700

    Update users routes
    I created the users api entry point

--- backend/server/routes/users.py
+++ backend/server/routes/users.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter, HTTPException
 from typing import List, Optional, Dict
 from beanie import PydanticObjectId
-from servers.models.user import User
+from server.models.user import User
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
+@router.get("/")
+async def get_all_users():
+  # return {"message":"you've reached the user endpoint."}
+  return await User.find_all().to_list()
