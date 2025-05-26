309f8e39e29d142dcb7b23b322359ccc6a2de395
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-26 14:11:45 -0700

    Update auth.py
    `hash_password` uses pwd_context.hash(password), which generates a random salt, hashes the password with that salt using bcrypt, and returns the final hash. Then, `verify_password` reads the salt from the stored hash and uses it to re-hash the input password, then compares the two.
    
    create_access_token creates a token for a specific user, and decode_access_token validates and extracts data from said token.

--- backend/server/services/auth.py
+++ backend/server/services/auth.py
@@ -1,1 +1,31 @@
+# services/auth.py
 # password hashing, token handling
+from passlib.context import CryptContext
+from datetime import datetime, timedelta, timezone
+from jose import jwt, JWTError
+from typing import Optional
+from pydantic import EmailStr
+from server.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
+import os
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.now(
+        timezone.utc) + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload.get("sub")
+    except JWTError:
+        return None
