7551f9878cb03706f9ccff95f9499982dc256329
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-26 13:53:08 -0700

    Update user schema
    I added a config class to userResponse to map ObjectId to str. I also added both UserUpdate and UserResponse classes.

--- backend/server/schemas/user.py
+++ backend/server/schemas/user.py
@@ -1,1 +1,32 @@
+# schemas/user.py
+
 # Register, login, profile
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+from datetime import datetime
+
+class UserCreate(BaseModel):
+  email: EmailStr
+  name: str
+  password: str
+
+class UserLogin(BaseModel):
+  email: EmailStr
+  password: str
+
+class UserResponse(BaseModel):
+  id: str
+  email: EmailStr
+  name: str
+
+  class Config:
+    from_attributes = True
+class UserUpdate(BaseModel):
+  email: EmailStr
+  name: str
+  password:str
+
+class UserPatch(BaseModel):
+  email: Optional[EmailStr] = None
+  name: Optional[str] = None
+  password: Optional[str] = None
