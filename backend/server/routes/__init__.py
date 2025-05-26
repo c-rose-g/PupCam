01103151547a8d216613569d4cfbebcf37ca7595
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-25 17:39:01 -0700

    Update __init__.py
    Added auth router

--- backend/server/routes/__init__.py
+++ backend/server/routes/__init__.py
@@ -1,5 +1,6 @@
+from .auth import router as auth_router
 from .devices import router as device_router
 from .events import router as event_router
 from .users import router as user_router
 
-__all__ = ["user_router","device_router", "event_router"]
+__all__ = ["auth_router","user_router","device_router", "event_router"]
