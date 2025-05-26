d8f97e05a8d90bc66c7df51feeeeea4642d8a34a
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-26 16:43:35 -0700

    Update events routes
    I added response_models to the routes, and updated the returns

--- backend/server/routes/events.py
+++ backend/server/routes/events.py
@@ -1,21 +1,46 @@
+# routes/events.py
 from fastapi import APIRouter, HTTPException
 from typing import List, Optional, Dict
 from beanie import PydanticObjectId
 from server.models.event import Event
-
+from server.schemas.event import EventCreate, EventResponse
 router = APIRouter(prefix="/events", tags=["Events"])
 
-@router.post("/{event_id}")
-async def create_event(event: Event):
-  await event.create()
-  return event
 
-@router.get("/")
+@router.post("/", response_model=EventResponse)
+async def create_event(event_data: EventCreate):
+    event = Event(**event_data.model_dump())
+    await event.create()
+
+    return EventResponse(
+        id=str(event.id),
+        event_type=event.event_type,
+        image_url=event.image_url,
+        video_url=event.video_url,
+        user_id=event.user_id,
+        timestamp=event.timestamp
+    )
+
+
+@router.get("/", response_model=List[EventResponse])
 async def get_all_events():
-  return await Event.find_all().to_list()
+    events = await Event.find_all().to_list()
+    return [EventResponse(id=str(event.id), event_type=event.event_type, image_url=event.image_url, video_url=event.video_url, user_id=event.user_id, timestamp=event.timestamp)
+            for event in events
+            ]
 
-@router.get("/{event_id}")
+
+@router.get("/{event_id}", response_model=EventResponse)
 async def get_event_by_id(event_id: str):
+    event = await Event.get(event_id)
 
-  event = await Event.get(event_id)
-  return event
+    if not event:
+        raise HTTPException(status_code=404, detail="Event not found")
+    return EventResponse(
+        id=str(event.id),
+        event_type=event.event_type,
+        image_url=event.image_url,
+        video_url=event.video_url,
+        user_id=event.user_id,
+        timestamp=event.timestamp,
+    )
