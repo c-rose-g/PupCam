585cff3e6211c6663046c39329e47a8baf49d8ef
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-26 16:41:36 -0700

    Update event model
    I removed field_validators, and I'm using HttpUrl instead. I deleted the TimeStamp class and added datetime logic inside the Event class. I also disinherited User and TimeStamp from the Event class. I also added user_id and timestamp to the sample object at the end

--- backend/server/models/event.py
+++ backend/server/models/event.py
@@ -1,16 +1,11 @@
+# models/event.py
 from datetime import datetime, timezone
 from typing import Optional, Literal
 from beanie import Document
-from pydantic import Field, ConfigDict, field_validator
+from pydantic import Field, ConfigDict, field_validator, HttpUrl
 from bson import ObjectId
 from server.models.user import User
-
-class TimeStamp(Document):
-    created_at: datetime = Field(
-        default_factory=lambda: datetime.now(timezone.utc))
-
-
-class Event(TimeStamp, User):
+class Event(Document):
     """
     Event model for storing motion detection events in the database.
 
@@ -20,26 +15,15 @@ class Event(TimeStamp, User):
     create partial index (id, created_at)
     """
     event_type: Literal["motion", "sound"]
-    image_url: str
-    video_url: str
-    user_id: str
-
-    @field_validator("image_url")
-    @classmethod
-    def validate_image_url(cls, value):
-        if not value.startswith("https://"):
-            raise ValueError("Image URL must start with 'https://'")
-        return value
-
-    @field_validator("video_url")
-    @classmethod
-    def validate_video_url(cls, value):
-        if not value.startswith("https://"):
-            raise ValueError("Video URL must start with 'https://'")
-        return value
-
+    image_url: HttpUrl
+    video_url: HttpUrl
+    user_id: Optional[str] = None
+    timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
     class Settings:
         name = "event_collection"
+
+    # class Config:
+    #     from_attributes = True
 
     model_config = ConfigDict(
         arbitrary_types_allowed=True,
@@ -49,7 +33,8 @@ class Event(TimeStamp, User):
                 "event_type": "motion",
                 "image_url": "https://example.com/image.jpg",
                 "video_url": "https://example.com/video.mp4",
-                "created_at": datetime.now()
+                "user_id" "test_dev_id"
+                "timestamp": datetime.now()
             }
         }
     )
