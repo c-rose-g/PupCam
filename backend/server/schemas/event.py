91701273c24fa12a8ae424ae43a98e5773628b59
Cindy Guzman <cindyrose.go@gmail.com>
2025-05-26 13:50:47 -0700

    Update event schema

--- backend/server/schemas/event.py
+++ backend/server/schemas/event.py
@@ -1,1 +1,13 @@
 # CreateEventRequest, EventResponse
+from pydantic import BaseModel
+from datetime import datetime
+
+class EventCreate(BaseModel):
+  type: str
+  snapshot_url: str
+
+class EventResponse(BaseModel):
+  id: str
+  type: str
+  timestamp: datetime
+  snapshot_url: str
