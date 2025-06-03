6e47e4a9e3ccdd5866993976233b4b04e966247c
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-03 11:52:11 -0700

    Update upload.py
    I changed the variable name for more consistency

--- backend/server/routes/upload.py
+++ backend/server/routes/upload.py
@@ -7,5 +7,5 @@ router = APIRouter(prefix="/upload", tags=["Upload"])
 @router.post("/")
 async def upload_video(file: UploadFile = File(...)):
     contents = await file.read()
-    file_id = await fs.upload_from_stream(file.filename, contents)
-    return JSONResponse({"video_url": str(file_id)})
+    video_url = await fs.upload_from_stream(file.filename, contents)
+    return JSONResponse({"video_url": str(video_url)})
