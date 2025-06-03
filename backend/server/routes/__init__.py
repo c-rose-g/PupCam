0f65da59dd6e7619e347d3c79b1725d03896ad61
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-03 11:22:53 -0700

    Update __init__.py
    I added the upload router

--- backend/server/routes/__init__.py
+++ backend/server/routes/__init__.py
@@ -2,5 +2,5 @@ from .auth import router as auth_router
 from .devices import router as device_router
 from .events import router as event_router
 from .users import router as user_router
-
-__all__ = ["auth_router","user_router","device_router", "event_router"]
+from .upload import router as upload_router
+__all__ = ["auth_router","user_router","device_router", "event_router", "upload_router"]
