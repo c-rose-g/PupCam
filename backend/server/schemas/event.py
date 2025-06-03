8f1e0a9335640d1b08761893905b9301fe16fd30
Cindy Guzman <cindyrose.go@gmail.com>
2025-06-03 11:51:53 -0700

    Update event schema
    I updated video_url to str

--- backend/server/schemas/event.py
+++ backend/server/schemas/event.py
@@ -6,14 +6,14 @@ from datetime import datetime, timezone
 class EventCreate(BaseModel):
   event_type: Literal["motion","sound"]
   image_url: HttpUrl
-  video_url: HttpUrl
+  video_url: str
   timestamp: datetime
 
 class EventResponse(BaseModel):
   id: str
   event_type: Literal["motion","sound"]
   image_url: HttpUrl
-  video_url: HttpUrl
+  video_url: str
 
   class Config:
     from_attributes = True
